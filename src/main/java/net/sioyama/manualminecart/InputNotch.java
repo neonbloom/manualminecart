5e7300e3e58b95beb547ae5ae9445bdfbb5b90f7
neonbloom <okasocot@icloud.com>
2026-06-01 23:45:46 +0900

    set済トロッコ判定を追加

--- src/main/java/net/sioyama/manualminecart/InputNotch.java
+++ src/main/java/net/sioyama/manualminecart/InputNotch.java
@@ -2,6 +2,9 @@ package net.sioyama.manualminecart;
 
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Minecart;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -18,18 +21,31 @@ public class InputNotch implements Listener {
 	@EventHandler
 	public void onPlayerInteract (PlayerInteractEvent e) {
 		if (e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK) {
+			// 棒かどうか
 			ItemStack item = e.getItem();
 			if (item != null && item.getType() == Material.STICK) {
 				if (!item.hasItemMeta()) {
 					return;
 				}
+				// mmc棒かどうか
 				ItemMeta meta = item.getItemMeta();
 				NamespacedKey key = new NamespacedKey(plugin, "manualminecartstick");
 				Integer value = meta.getPersistentDataContainer().get(key, PersistentDataType.INTEGER);
 				if (value == null || value != 1) {
 					return;
 				}
-			
+				// set済トロッコかどうか
+				Player player = e.getPlayer();
+				Entity vehicle = player.getVehicle();
+				if (!(vehicle instanceof Minecart minecart)) {
+					return;
+				}
+				NamespacedKey minecartKey = new NamespacedKey(plugin, "manualminecart");
+				Integer minecartValue = minecart.getPersistentDataContainer().get(minecartKey, PersistentDataType.INTEGER);
+				if (minecartValue == null || minecartValue != 1) {
+					return;
+				}
+				player.sendMessage("クリック検知、mmc棒、set済");
 			}
 		}
 	}
--- src/main/java/net/sioyama/manualminecart/ManualMinecart.java
+++ src/main/java/net/sioyama/manualminecart/ManualMinecart.java
@@ -22,6 +22,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ManualMinecart extends JavaPlugin implements Listener {
 	Map<UUID, TrainState> ts = new HashMap<>();
+	public TrainState setTrainState(UUID uuid) {
+		TrainState state = ts.get(uuid);
+		if (state == null) {
+			state = new TrainState();
+			ts.put(uuid, state);
+		}
+		return state;
+	}
 	
 	@Override
 	public void onEnable() {
